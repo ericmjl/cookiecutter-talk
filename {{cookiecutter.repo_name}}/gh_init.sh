94c2e36732ea69d4df7292c244e14d6e21ca5513
Eric J. Ma <ericmajinglong@gmail.com>
2016-12-15 15:35:53 -0500

    updated gh_init.sh script to work

--- {{cookiecutter.repo_name}}/gh_init.sh
+++ {{cookiecutter.repo_name}}/gh_init.sh
@@ -1,5 +1,8 @@
-# This script will run the necessary git commands to point to GitHub 
+# This script will run the necessary git commands to point to GitHub
 
 git remote add origin git@github.com:{{cookiecutter.github_username}}/{{cookiecutter.repo_name}}
+
+git add .
+git commit -m "first commit"
 git push -u origin master
-rm initialize_upload.sh
+rm gh_init.sh
